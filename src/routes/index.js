ceaf9a1e8dc99f0c12220386b8a802bf9c0242f8
22120197 <nguyenvinhluong242004@gmail.com>
2025-03-24 08:57:38 +0700

    update connect tiktok

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,14 @@ const home = require('./home.route');
 const detailMovie = require('./detail-movie.route');
 const watchMovie = require('./watch-movie.route');
 const search = require('./search.route');
+const path = require("path");
 
 function route(app) {
-    
+
+    app.use("/tiktokihsyNtbiXnl4eQIZ8G14VFmfGJwpgroK.txt", (req, res) => {
+        res.sendFile(path.join(__dirname, "../app/config/tiktokihsyNtbiXnl4eQIZ8G14VFmfGJwpgroK.txt"));
+    });
+
     app.use('/watch-movie', watchMovie);
     
     app.use('/detail-movie', detailMovie);
